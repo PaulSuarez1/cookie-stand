370464a33162826cd23ee90a3faa101298910cb9
Paul Suarez <paulsblogging.com@gmail.com>
2018-05-07 18:29:29 -0700

    got the second array finished

--- app.js
+++ app.js
@@ -7,18 +7,11 @@ var firstAndPike = {
     name: '1st and Pike',
     minCust: 23,
     maxCust: 65,
-    totalCust: null,
+    totalCust: 0,
     avgCookPerCust: 6.3,
     custPerHour: [],
     cookPerHour: [],
-    totalForDay: null,
-    render: function () {
-        var list = document.getElementById('first-and-pike');
-        var newLi = document.createElement('li');
-        newLi.textContext = this;
-        list.appendChild(newLi);
-    }
-    
+    totalForDay: 0,
 }
     
 firstAndPike.custPerHourFunct = function () {
@@ -26,7 +19,29 @@ firstAndPike.custPerHourFunct = function () {
         var random = Math.floor(Math.random() * (this.maxCust - this.minCust + 1)) + this.minCust;
         this.custPerHour.push(random);
         }
-    
-}
+    }
+
+firstAndPike.cookPerHourFunct = function () {
+    for ( var j = 0; j < hours.length; j++) {
+        var arrayTimesAvg = Math.floor(firstAndPike.custPerHour[j] * firstAndPike.avgCookPerCust);
+        this.cookPerHour.push(arrayTimesAvg);
+        firstAndPike.totalForDay += arrayTimesAvg;
+        }
+    }
+
+firstAndPike.renderList = function () {
+    var list = document.getElementById('firstAndPike');
+    for (var h = 0; h < this.cookPerHour.length; h++) {
+        var newLi = document.createElement('li');
+        newLi.textContent = '' + hours[h] + ': ' + this.cookPerHour[h] + ' cookies';
+        list.appendChild(newLi);
+        }
+    }
+
 firstAndPike.custPerHourFunct();
-console.log(firstAndPike.custPerHour);
+firstAndPike.cookPerHourFunct();
+firstAndPike.renderList();
+
+console.log('this is our array for customers per hour ' + firstAndPike.custPerHour);
+console.log('this is our array for cookies being sold per hour in total ' + firstAndPike.cookPerHour);
+console.log('this is how many in total we sold for the day ' + firstAndPike.totalForDay);
